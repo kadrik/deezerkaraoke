7506e635f88dfd24dc91304faad9ee9d9875feaa
cat <cat@cat.local>
2013-04-20 20:53:34 +0200

    lyrics qui fonctionnent

--- lyrics.js
+++ lyrics.js
@@ -2,44 +2,40 @@
 var API_KEY="82be3ea3f79ea404d45f47607c103eff";
 	var lyrics = new Array();
 
-function getTrackLyrics(title, artist, handlerloaded)
+function getTrackLyrics(title, artist, time, handlerloaded)
 {
     $.ajax({
-	       dataType:"jsonp",
-	       async: false,
-	       url :"http://api.musixmatch.com/ws/1.1/matcher.subtitle.get",
-	       data :{
-		   apikey: API_KEY,
-		       format: "jsonp",
-		       q_track: title,
-		       q_artist: artist,
-		       //f_subtitle_length:300,
-		       //f_subtitle_length_max_deviation: 500,
-		       subtitle_format:"mxm"
-	     }, 
-	    success:function(data) {
-		   if (data.message.body == null) {
+	        dataType:"jsonp",
+		async: false,
+		url :"http://api.musixmatch.com/ws/1.1/matcher.subtitle.get",
+		data :{
+		    apikey: API_KEY,
+		    format: "jsonp",
+		    q_track: title,
+		    q_artist: artist,
+		    f_subtitle_length:time,
+		    f_subtitle_length_max_deviation: 10,
+		    subtitle_format:"mxm"
+		    }, 
+		success:function(data) {
+		    if (data.message.body == null) {
 			//Track not found, body is null
 			console.log("Track "+ title + " " + artist + " not found, body is null");
 			lyrics = null;
-   			}
-		   else {
+		    }
+		    else {
 			//Found the track, body not null
 			lyrics = JSON.parse(data.message.body.subtitle.subtitle_body);
-			console.log("current track lyrics", lyrics);
-			console.log("Track "+ title + " " + artist + " FOUND");
-			}
-		   //$("#debug").append(data);
-	     },
-	   error:function (data) {
-		console.log("ERROR ", data);
-		   //$("#debug").append(data);
-	    },
-	   complete:function (data) {
+		    }
+	        },
+		error:function (data) {
+		    console.log("ERROR ", data);
+	        },
+		complete:function (data) {
 		handlerloaded();
 	    }
-
-	  });
+	    
+	});
 }
 
 function getWholeLyrics()
@@ -56,6 +52,10 @@ function getWholeLyrics()
 
 function getLyricsWithTime(time)
 {
-
+    for (var i = 0; i < lyrics.length; i++) {
+	var part = lyrics[i];
+	if ((part.time > time))// && (part.time > time))
+	    return part.test;
+    }
 }
 
